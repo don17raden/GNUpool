4ffb6ae87aaa6f1452b893f5ffdfe24fa634456a
askelad0 <askelad0@proton.me>
2023-03-15 21:40:03 +0530

    Initial front end

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from "next";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+  const [walletAddress, setWalletAddress] = useState("");
+
+  async function requestAccount() {
+    //console.log("Inside Function requestAccount");
+
+    if (window.ethereum) {
+      //console.log("Detected Metamsk");
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        console.log(accounts);
+        setWalletAddress(accounts[0]);
+        router.push("/team");
+    } else {
+      alert("Please install metamask");
+    }
+  }
+
+
+  return (
+    <div className="pl-2 font-poppins">
+      <div className="flex flex-wrap h-screen flex-row justify-between">
+        <div className="flex flex-col w-6/12">
+          <div className="flex flex-col justify-center px-20 gap-5">
+            <p className="font-black text-2xl py-4">GNOD</p>
+            <h1 className="text-7xl font-semibold text-[#6565d6] mt-16 leading-[90px]">
+              <span className="font-black text-[#3661EB]">Stake</span> GNO{" "}
+              as a Team
+            </h1>
+            <p className="text-2xl w-full text-[#888B94] font-normal">
+              Create or Join a team and stake together
+            </p>
+            <br/>
+            <button
+              className="bg-[#3661EB] w-full h-14 mt-4 text-slate-100 font-normal rounded-md"
+              onClick={requestAccount}
+            >
+              Connect wallet
+            </button>
+            <button className="bg-[#e9ecee] w-full h-14 text-[#80858B] font-medium rounded-md">
+              Learn more about metamask policies
+            </button>
+          </div>
+        </div>
+        <div className="w-6/12 bg-[#7bbf8e] h-screen overflow-hidden">
+          <img src="/cover-overlay.png" className="h-full object-contain"></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/pages/team.tsx
@@ -0,0 +1,105 @@
+
+
+  export default function dash() {
+
+    return (
+        <div className="font-poppins ">
+        {/* <div className="font-poppins ml-20">
+            <p className="font-black text-2xl pt-2">GNOD</p>
+        </div> */}
+
+        <center>
+            <h1 className="text-7xl font-semibold   leading-[90px]">
+                <span className="font-black text-[#4fd4aa]">GNOD</span>
+            </h1>   
+            
+            <button type="button" className="mb-3 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Already have a pack</button>
+
+
+            <div className="w-1/2 bg-[#1a5550] rounded h-44 ">
+                <div className="flex flex-row">
+                    <div className=" basis-1/2">
+                        <p className="text-green-300 font-bold text-1xl pt-3">Stake GNO</p>
+                    </div>
+                    <div className="basis-1/2"></div>
+                    <div className="basis-1/2 ">
+                        <div className="flex">
+                            <p className="justify-end font-bold text-green-300 text-1xl pt-3">Balance : 0.0 GNO</p>
+                            <button type="button" className="ml-5 mt-2 bg-white border border-green-300 focus:outline-none hover:bg-green-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-3 py-1 dark:bg-green-800 dark:text-green-200 dark:border-green-600 dark:hover:bg-green-700 dark:hover:border-green-600 dark:focus:ring-gray-700">Max</button>
+                        </div>
+                    </div>
+                </div>
+
+                <form className="pt-2">   
+                    <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <img src='./cover.png' className="h-8 w-8"  />
+                        </div>
+                        <input type="number"  className=" block w-full text-2xl p-4 pl-14  text-green-900 border border-green-300 rounded-lg  bg-gray-700 focus:ring-green" placeholder="0.0" required />
+                        <h2 className="text-white absolute right-3 bottom-4 text-2xl"> ≈ $45.2 USD</h2>
+                    </div>
+                </form>
+            </div>
+            <center>
+            <div className="items-center flex mt-6 z-40 justify-center ">
+                <button type="button" className=" absolute bg-[#14b8a6] hover:bg-[#5eead4] font-medium rounded-full text-sm px-4 py-4 ">
+                    <img src='./exch.svg' className="h-10 w-10"></img>
+                </button>
+            </div>
+            </center>
+            <div className="w-1/2 h-92 mt-8  bg-[#1a5550]">
+                <div className="flex flex-row pt-4 mb-3">
+                    <div className=" basis-1/2">
+                        <p className=" text-green-300 font-bold text-1xl pt-3">Receive GNO</p>
+                    </div>
+                    <div className="basis-1/2"></div>
+                    <div className="basis-1/2 ">
+                        <div className="flex">
+                            <p className="justify-end font-bold  text-green-300 text-1xl pt-3">Balance : 0.00 GNO</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mr-2 ml-2 p-5 rounded  bg-gray-700">
+                    <div className="flex justify-between">
+                        <div className="relative inset-y-0 left-0 flex items-center  pointer-events-none">
+                            <img src='./cover.png' className="h-8 w-8"  />
+                        </div>
+                        <p className="pt-1 text-2xl">34 GNO</p>
+                        <div className="basis-1/4"></div>
+                        <div className="flex justify-end basis-1/2">
+                            <p className="text-2xl">≈ $45.2 USD</p>
+                        </div>
+                    </div> 
+                </div>
+                <hr className="h-px mt-4 w-4/5 bg-gray-200 border-0 dark:bg-gray-700"/>
+                <div className="mr-2 p-3">
+                    <div className="flex justify-between">
+                        <p className="pt-1 text-1xl">Average Return</p>
+                        <div className="basis-1/4"></div>
+                        <div className="flex justify-end basis-1/2">
+                            <p className="text-1xl">≈ 5.82% APR</p>
+                        </div>
+                    </div> 
+                </div>
+                <hr className="h-px mt-4 w-4/5 bg-gray-200 border-0 dark:bg-gray-700"/>
+                <div className="mr-2 p-3">
+                    <div className="flex justify-between">
+                        <p className="pt-1 text-1xl">Transaction Cost</p>
+                        <div className="basis-1/4"></div>
+                        <div className="flex justify-end basis-1/2">
+                            <p className="text-1xl">0.01920 ETH (≈ $34.04 USD)</p>
+                        </div>
+                    </div> 
+                </div>
+
+                <div className="pt-5 pb-5">
+                    <p className=" text-green-500">Help Center</p>
+                </div>
+            </div>
+            </center>
+            <br/>
+        </div>
+    );
+  }
+  
