5d4adde3a1ce84a4c23bd825b181aa6fc5344d5d
cryptobugbear <cryptobugbear>
2023-03-17 22:01:32 +0800

    Add additional docs

---
+++ safe/Readme.md
@@ -0,0 +1,3 @@
+# Goals
+1. Allow friends to create a mini pool and join rocket pool as a single node operator
+2. Use oSnap to vote to withdraw
--- safe/safe_commands.sh
+++ safe/safe_commands.sh
@@ -1,2 +1,10 @@
 #Create a new mini pool wallet
 safe-creator https://rpc.chiadochain.net $PRIVATE_KEY --owners 0xFF8F016D5702113fA734ea78f168c0BF8c059797 --threshold 1
+
+#enable Zodiac module
+enable_module 
+
+#enable oSnap module
+enable_module 
+
+#Create snapshot 
